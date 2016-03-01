8a7a9089cdc5a313f24a05c80c7c2ad02a01ebad
Witold Markowski <witold.a.markowski@gmail.com>
2016-03-01 23:56:15 +0200

    Use the minimal output power (easier for flooder testing).

--- src/interface/RF24Interface.cpp
+++ src/interface/RF24Interface.cpp
@@ -33,7 +33,7 @@ bool RF24Interface::up()
 
 	rf24.openWritingPipe(linkAddress);
 	rf24.openReadingPipe(1, linkAddress);
-	rf24.setPALevel(RF24_PA_MAX);
+	rf24.setPALevel(RF24_PA_MIN);
 	rf24.setAutoAck(false);
 	rf24.setPayloadSize(PAYLOAD_SIZE);
 	rf24.openWritingPipe(linkAddress);
